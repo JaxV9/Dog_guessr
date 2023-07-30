7d7583cf3903c26f8e17b95432d0f0e728e476f3
LAYAN_Jason <layanj.pro@gmail.com>
2023-07-30 18:17:34 +0200

    refacto

--- backend/index.js
+++ backend/index.js
@@ -29,10 +29,3 @@ app.get('/scores', (req, res) => { //On retourne la liste des scores présent da
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
-
-/*
- Cette API permet de:
-- 1) ajouter un nouveau score dans un fichier (nommé scores.txt ou scores.json) côté serveur lors d'un appel d'API du type /scores/add/9 -> ajoute 9 à la suite du fichier
-- 2) retourner la liste des scores présent dans le fichier de scores lors d'un appel d'API du type /scores
-- (en option) on pourra relier l'api à notre application frontend dans de sauvegarder le score des joueurs et d'afficher le meilleur score et le nombre de parties jouées dans le dashboard
-*/
